7bc259e37258f7b4e96a6b8cfd9f4c0312cb695e
Ca20009 <134835628+Ca20009@users.noreply.github.com>
2024-12-11 12:36:55 +0000

    Create linpeas.sh

---
+++ linpeas.sh
@@ -0,0 +1,11 @@
+whoami
+cat /etc/shadow
+ls /root/.ssh
+set | grep aws
+find /home -name "credentials"
+cat /etc/issue
+cat /etc/*-release
+cat /etc/lsb-release
+cat /etc/redhat-release
+cat /etc/cron*
+cat /etc/cron.d/
